d8ee2fc080ccde249d0586092e50abc1c8bdfe72
Ganesh Dutt <gdutt2304@gmail.com>
2024-04-14 09:32:47 +0530

    class 12pm

---
+++ main.js
@@ -0,0 +1,22 @@
+function init() {
+    alert("hello world!")
+   }
+document.writeln("<br>");
+
+
+function fun(){
+    document.writeln("fun ");
+    
+}
+fun();
+fun();
+fun();
+fun();
+fun();
+document.writeln("<br>");
+let a = 10;
+
+for(let i = 0; i<a; i++){
+  document.writeln("fun");
+  
+}
